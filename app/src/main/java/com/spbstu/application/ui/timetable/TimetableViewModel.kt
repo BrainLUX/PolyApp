db86859903be6cbb457631520acffaab9b99dafa
Shabinsky Dmitry <sdimosikvip@gmail.com>
2021-11-04 05:47:37 +0300

    add broke data pick

--- app/src/main/java/com/spbstu/application/ui/timetable/TimetableViewModel.kt
+++ app/src/main/java/com/spbstu/application/ui/timetable/TimetableViewModel.kt
@@ -4,13 +4,20 @@ import com.spbstu.application.base.BaseViewModel
 import com.spbstu.application.domain.model.Lesson
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import java.util.*
 
 class TimetableViewModel : BaseViewModel() {
 
     private val _testDay: MutableStateFlow<List<Lesson>> = MutableStateFlow(listOf())
     val testDay get() :StateFlow<List<Lesson>> = _testDay
 
+    private val _pickData: MutableStateFlow<Calendar> = MutableStateFlow(Calendar.getInstance())
+    val pickData: StateFlow<Calendar> = _pickData
+
     init {
+
+        _pickData.value = Calendar.getInstance()
+
         _testDay.value =
             listOf(
                 Lesson(
@@ -32,5 +39,19 @@ class TimetableViewModel : BaseViewModel() {
                     "Чел"
                 )
             )
+    }
+
+    fun updatePickedDate(calendar: Calendar) {
+        _pickData.value = calendar
+    }
+
+    fun updateToNextWeek() {
+        var i: Int = _pickData.value.get(Calendar.WEEK_OF_MONTH)
+        _pickData.value.set(Calendar.WEEK_OF_MONTH, ++i)
+    }
+
+    fun updateToPrevWeek() {
+        var i: Int = _pickData.value.get(Calendar.WEEK_OF_MONTH)
+        _pickData.value.set(Calendar.WEEK_OF_MONTH, --i)
     }
 }
